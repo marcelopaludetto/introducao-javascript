77ae8825fa71ebf6bd51ebc2240d823bc7645041
Marcelo Paludetto Rodrigues <78444171+marcelopaludetto@users.noreply.github.com>
2022-01-02 16:16:58 -0300

    curso finalizado

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -7,7 +7,7 @@ botaoAdicionar.addEventListener("click", function () {
     xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-
+    var erroAjax = document.querySelector("#erro-ajax");
         if (xhr.status == 200){
             erroAjax.classList.add("invisivel");
             var resposta = xhr.responseText;
@@ -21,7 +21,7 @@ botaoAdicionar.addEventListener("click", function () {
         } else{
             console.log(xhr.status);
             console.log(xhr.responseText);
-            var erroAjax = document.querySelector("#erro-ajax");
+           
             erroAjax.classList.remove("invisivel");
         }
 
